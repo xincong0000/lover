63be9a73fb614155cdac78a5b9fa2bee478b2cdf
SleepingElf <1164293596@qq.com>
2020-04-02 10:50:53 +0800

    修正首页样式

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -3,12 +3,18 @@
 namespace app\index\controller;
 
 use app\common\controller\Index as Cindex;
+
 class Index extends Cindex
 {
     /**
      * just for you
      */
     public function index()
+    {
+        return $this->fetch();
+    }
+
+    public function detail()
     {
         return $this->fetch();
     }
