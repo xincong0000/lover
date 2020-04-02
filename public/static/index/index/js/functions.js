68f629d72f3ad1e8c847beca9e8c4364ef32fecf
SleepingElf <1164293596@qq.com>
2020-04-02 13:55:48 +0800

    cs

--- public/static/index/index/js/functions.js
+++ public/static/index/index/js/functions.js
@@ -5,9 +5,8 @@ $(function () {
     $.fn.snow({
         minSize: 5,		//雪花的最小尺寸
         maxSize: 50, 	//雪花的最大尺寸
-        newOn: 150		//雪花出现的频率 这个数值越小雪花越多
+        newOn: 300		//雪花出现的频率 这个数值越小雪花越多
     });
-    
 
     $loveHeart = $("#loveHeart");
     var a = $loveHeart.width() / 2;
