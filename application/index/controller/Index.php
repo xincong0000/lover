cb072854a560a1c06aca3d4c67a9d23e1bcf37ea
SleepingElf <1164293596@qq.com>
2020-04-09 15:53:47 +0800

    '修正首页'

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,10 +8,10 @@ class Index extends Cindex
 {
     public function index()
     {
-        $topList = model('LoverHome')->where(['type' => 1])->field(['content', 'id'])->order(['sort', 'id'])->select();
-        $footerList = model('LoverHome')->where(['type' => 2])->field(['content', 'id'])->order(['sort', 'id'])->select();
-        $this->assign('topList', $topList);
-        $this->assign('footerList', $footerList);
+//        $topList = model('LoverHome')->where(['type' => 1])->field(['content', 'id'])->order(['sort', 'id'])->select();
+//        $footerList = model('LoverHome')->where(['type' => 2])->field(['content', 'id'])->order(['sort', 'id'])->select();
+//        $this->assign('topList', $topList);
+//        $this->assign('footerList', $footerList);
         return $this->fetch();
     }
 }
